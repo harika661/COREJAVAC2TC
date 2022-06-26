2e665c505bf968dd8e70b7d5c72689885126b66e
RREDD <RREDD@DESKTOP-G6BJ8U8>
2022-06-26 22:35:01 +0530

    switchcase

---
+++ javakeywords/src/javakeywords/ifelse.java
@@ -0,0 +1,32 @@
+package javakeywords;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=5;
+if(x==5)
+{
+	if(x>=5)
+	{
+		System.out.println("X is greater than 5");
+	}
+	else
+	{
+		System.out.println("X is smaller than 5");
+	}
+	}
+if(x==5)
+{
+	System.out.println("X is greater to 5");
+}
+else
+{
+	System.out.println("X is smaller than 5");
+}
+	}
+}
+
+	
+
+
---
+++ javakeywords/src/javakeywords/switchcaseString.java
@@ -0,0 +1,26 @@
+package javakeywords;
+
+public class switchcaseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name="TEAM";
+switch
+(name.toLowerCase())
+{
+case "author":
+	System.out.println("Vikas");
+	break;
+case "team":
+	System.out.println("Team Java Full Stack");
+	break;
+case "editor":
+	System.out.println("Vishnu&Krishna");
+	break;
+	default:
+		System.out.println("Invalid entry");
+		break;
+}
+	}
+
+}
