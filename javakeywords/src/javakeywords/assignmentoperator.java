06f0d20fe3372c4b0364657ccc062e6df1b10854
RREDD <RREDD@DESKTOP-G6BJ8U8>
2022-06-26 16:15:25 +0530

    assignment operator

---
+++ javakeywords/src/javakeywords/assignmentoperator.java
@@ -0,0 +1,16 @@
+package javakeywords;
+
+public class assignmentoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=4;
+//x+=5;
+//x-=5;
+//x*=5;
+//x/=4;
+x%=5;
+System.out.println(x);
+	}
+
+}
