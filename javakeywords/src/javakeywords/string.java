2ae0a462bbc6d9a1e083c1d25e47ba75afcb77d6
RREDD <RREDD@DESKTOP-G6BJ8U8>
2022-06-26 15:27:04 +0530

    typecasting

---
+++ javakeywords/src/javakeywords/string.java
@@ -0,0 +1,11 @@
+package javakeywords;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String var=new String("Hello World");
+System.out.println(var);
+	}
+
+}
---
+++ javakeywords/src/typecasting.java
@@ -0,0 +1,11 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+long l=(long)i;
+System.out.println(l);
+	}
+
+}
