ceacf836601c8633eaa21615de94a29d0352792a
RREDD <RREDD@DESKTOP-G6BJ8U8>
2022-06-26 16:10:04 +0530

    operators

---
+++ javakeywords/src/decisionmaking2.java
@@ -0,0 +1,17 @@
+
+public class decisionmaking2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10;int y=11;
+if(!(x<y)||(x==y))
+{
+	System.out.println("condition is true");
+}	
+else
+{
+	System.out.println("condition is false");
+}
+	}
+
+}
---
+++ javakeywords/src/terneryoperator.java
@@ -0,0 +1,11 @@
+
+public class terneryoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=11;
+x=(10==x)?1:0;
+System.out.println(x);
+	}
+
+}
