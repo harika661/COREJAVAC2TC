d18c6a7bedccf0fe6667dd512f731bbeff4fad33
RREDD <RREDD@DESKTOP-G6BJ8U8>
2022-06-27 09:35:01 +0530

    Control Statements

---
+++ javakeywords/src/javakeywords/Forloop.java
@@ -0,0 +1,16 @@
+package javakeywords;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		for(int i=1;i<=1000;i++)
+		{
+			System.out.println("Value of i:");
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
