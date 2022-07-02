28a3aa7ad68e43afd8f3a7376a66e9af7fc0e173
RREDD <RREDD@DESKTOP-G6BJ8U8>
2022-07-02 10:33:59 +0530

    Single inheritance

---
+++ javakeywords/src/com/svkp/inheritance/Singleinheritance.java
@@ -0,0 +1,15 @@
+package com.svkp.inheritance;
+class parent
+{
+	void display()
+	{
+		System.out.println("This is parent class");
+	}
+}
+public class Singleinheritance extends parent{
+public static void main(String[] args) {
+		Singleinheritance obj=new Singleinheritance();
+obj.display();
+	}
+
+}
