4c4de0eb282b5d75e740a364465edb2b65aff417
RREDD <RREDD@DESKTOP-G6BJ8U8>
2022-08-15 17:09:20 +0530

    Exception handling

---
+++ javakeywords/src/Exception.java
@@ -0,0 +1,15 @@
+
+public class Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+try {
+	throw new NullPointerException("Hello");
+}
+catch(ArithmeticException e)
+{
+	System.out.print("B");
+}
+	}
+
+}
